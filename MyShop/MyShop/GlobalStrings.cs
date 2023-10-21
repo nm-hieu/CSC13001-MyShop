1404af2a5ac29e23adf7c19105ce39c4362eb87f
qhungo0125 <hungngojan25@gmail.com>
2023-10-21 15:16:20 +0700

    init template UI for 4 tags

---
+++ MyShop/MyShop/GlobalStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop
+{
+    public static class GlobalStrings
+    {
+        public static string AppName{ get; } = "My Shop";
+        public static string WelcomeText { get; } = "Welcome to My Shop";
+    }
+}
---
+++ MyShop/MyShop/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MyShop.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.MVVM.ViewModel
+{
+    internal class MainViewModel: Observable
+    {
+        private object _currentView;
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ProductsViewCommand { get; set; }  
+        public HomeViewModel HomeVM { get; set; }
+        public ProductsViewModel ProductsVM { get; set; }
+        public object CurrentView
+        {
+            get { return _currentView;}
+            set { _currentView = value;}
+        }
+
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            ProductsVM = new ProductsViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            ProductsViewCommand= new RelayCommand(o =>
+            {
+                CurrentView = ProductsVM;
+            });
+
+
+
+
+        }
+    }
+}
