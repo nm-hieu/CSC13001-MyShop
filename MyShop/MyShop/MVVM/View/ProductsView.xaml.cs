2fc96248aec86ec23276955685ecd45e570e70ed
qhungo0125 <hungngojan25@gmail.com>
2023-10-31 17:11:46 +0700

    refactor to MVVM structure

--- MyShop/MyShop/MVVM/View/ProductsView.xaml.cs
+++ MyShop/MyShop/MVVM/View/ProductsView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using MyShop.MVVM.Model;
+using MyShop.MVVM.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,12 +27,8 @@ namespace MyShop.MVVM.View
     /// </summary>
     public partial class ProductsView : UserControl
     {
-        BindingList<Product> _products;
-        BindingList<Category> _categories;
-        int _rowsPerPage = 5;
-        int _totalPages = -1;
-        int _totalItems = -1;
-        int _currentPage = 1;
+        ProductsViewModel ProductsVM;
+
         public ProductsView()
         {
             InitializeComponent();
@@ -38,127 +36,15 @@ namespace MyShop.MVVM.View
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+            ProductsVM = new ProductsViewModel();
+            ProductsVM.loadData();
+            productsView.ItemsSource = ProductsVM._products;
         }
 
-        private string selectTable (string tableName)
-        {
-            var sql = $"select *, count(*) over() as Total from {tableName} ";
-            return sql;
-        }
-
-        private string addWhere (string baseSQL, string param)
-        {
-            string sql = baseSQL;
-            sql += "where ";
-            return sql;
-        }
-
-        private void LoadCategories()
-        {
-            string tableName = "Products";
-            var sql = selectTable(tableName);
-            _categories = new BindingList<Category>();
-
-
-            var command = new SqlCommand(sql, DB.Instance.Connection);
-
-            var reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                int id = (int)reader["ID"];
-                string name = (string)reader["Name"];
-
-                var category = new Category()
-                {
-                    ID = id,
-                    Name = name,
-                };
-                _categories.Add(category);
-            }
-            reader.Close();
-        }
-
-        private void LoadAllProducts()
-        {
-            string tableName = "Products";
-            var sql = selectTable(tableName);
-
-            var command = new SqlCommand(sql, DB.Instance.Connection);
-
-            var reader = command.ExecuteReader();
-            _products = new BindingList<Product>();
-            int count = -1;
-
-            while (reader.Read())
-            {
-                int id = (int)reader["ID"];
-                string name = (string)reader["Name"];
-                int price = (int)reader["Price"];
-                int category = (int)reader["Category"];
-                string image = (string)reader["Image"];
-                string color = (string)reader["Color"];
-
-                var product = new Product()
-                {
-                    ID = id,
-                    Name = name,
-                    Price = price,
-                    Image = image,
-                    Color = color,
-                    Category = category
-                };
-                _products.Add(product);
-                _totalItems = (int)reader["Total"];
-                count = (int)reader["Total"];
-            }
-            reader.Close();
-            productsView.ItemsSource = _products;
-            // connect to itemsSource here
-
-        }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            loading.IsIndeterminate = true;
-            var builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "HUNGLEGION\\SQLSERVER";
-            builder.InitialCatalog = "Project1";
-            builder.TrustServerCertificate = true;
-            builder.IntegratedSecurity = true;
-
-
-            string connectionString = builder.ConnectionString;
-
-            var connection = new SqlConnection(connectionString);
-            connection = await Task.Run(() => {
-                var _connection = new SqlConnection(connectionString);
-                try
-                {
-                    _connection.Open();
-                }
-                catch (Exception ex)
-                {
-
-                    _connection = null;
-                }
-                return _connection;
-            });
-
-            loading.IsIndeterminate = false;
-
-            if (connection != null)
-            {
-                DB.Instance.ConnectionString = connectionString;
-            }
-            else
-            {
-                MessageBox.Show(
-                    $"Cannot connect to DB"
-                );
-            }
-            LoadCategories();
-            LoadAllProducts();
-
+            
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
--- MyShop/MyShop/MVVM/ViewModel/ProductsViewModel.cs
+++ MyShop/MyShop/MVVM/ViewModel/ProductsViewModel.cs
@@ -1,14 +1,138 @@
-﻿using MyShop.Core;
+﻿using Microsoft.Data.SqlClient;
+using MyShop.Core;
+using MyShop.MVVM.Model;
+using MyShop.MVVM.View;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MyShop.MVVM.ViewModel
 {
     class ProductsViewModel
     {
-        public ProductsViewModel() { }
+        public BindingList<Product> _products { get; set; }
+        public BindingList<Category> _categories { get; set; }
+        public int _rowsPerPage = 5;
+        public int _totalPages = -1;
+        public int _totalItems = -1;
+        public int _currentPage = 1;
+        public ProductsViewModel() {
+            ConnectToDB();
+        }
+
+        public void loadData()
+        {
+            LoadAllProducts();
+            LoadCategories();
+        }
+
+        public async void ConnectToDB ()
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "HUNGLEGION\\SQLSERVER";
+            builder.InitialCatalog = "Project1";
+            builder.TrustServerCertificate = true;
+            builder.IntegratedSecurity = true;
+
+
+            string connectionString = builder.ConnectionString;
+
+            var connection = new SqlConnection(connectionString);
+            connection = await Task.Run(() => {
+                var _connection = new SqlConnection(connectionString);
+                try
+                {
+                    _connection.Open();
+                }
+                catch (Exception ex)
+                {
+
+                    _connection = null;
+                }
+                return _connection;
+            });
+
+            if (connection != null)
+            {
+                DB.Instance.ConnectionString = connectionString;
+            }
+            else
+            {
+                MessageBox.Show(
+                    $"Cannot connect to DB"
+                );
+            }
+        }
+        private string selectTable(string tableName)
+        {
+            var sql = $"select *, count(*) over() as Total from {tableName} ";
+            return sql;
+        }
+
+        public void LoadAllProducts()
+        {
+            string tableName = "Products";
+            var sql = selectTable(tableName);
+
+            var command = new SqlCommand(sql, DB.Instance.Connection);
+
+            var reader = command.ExecuteReader();
+            _products = new BindingList<Product>();
+            int count = -1;
+
+            while (reader.Read())
+            {
+                int id = (int)reader["ID"];
+                string name = (string)reader["Name"];
+                int price = (int)reader["Price"];
+                int category = (int)reader["Category"];
+                string image = (string)reader["Image"];
+                string color = (string)reader["Color"];
+
+                var product = new Product()
+                {
+                    ID = id,
+                    Name = name,
+                    Price = price,
+                    Image = image,
+                    Color = color,
+                    Category = category
+                };
+                _products.Add(product);
+                _totalItems = (int)reader["Total"];
+                count = (int)reader["Total"];
+            }
+            reader.Close();
+            // connect to itemsSource here
+
+        }
+
+        public void LoadCategories()
+        {
+            string tableName = "Products";
+            var sql = selectTable(tableName);
+            _categories = new BindingList<Category>();
+            var command = new SqlCommand(sql, DB.Instance.Connection);
+
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = (int)reader["ID"];
+                string name = (string)reader["Name"];
+
+                var category = new Category()
+                {
+                    ID = id,
+                    Name = name,
+                };
+                _categories.Add(category);
+            }
+            reader.Close();
+        }
     }
 }
