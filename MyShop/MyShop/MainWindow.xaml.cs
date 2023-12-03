61efde78509010ef6ba02b6c6001cabaaf129021
qhungo0125 <hungngojan25@gmail.com>
2023-12-03 22:34:27 +0700

    responsive UI

--- MyShop/MyShop/MainWindow.xaml.cs
+++ MyShop/MyShop/MainWindow.xaml.cs
@@ -52,29 +52,6 @@ namespace MyShop
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            int cellsPerRow = 4;
-            toggleButton.IsChecked = (false) ; // show sidebar
-
-            if (this.ActualWidth < 1200 && this.ActualWidth >= 900)
-            {
-                toggleButton.IsChecked = (false); // show sidebar
-                cellsPerRow = 3;
-            } else if (this.ActualWidth >= 600 && this.ActualWidth < 900)
-            {
-                cellsPerRow = 2;
-                toggleButton.IsChecked = (true); // hidden sidebar
-            }
-
-            GridView gridView = new GridView();
-            for (int i = 0; i < cellsPerRow; i++)
-            {
-                gridView.Columns.Add(new GridViewColumn
-                {
-                    Header = "Product " + (i + 1),
-                    Width = 150, // Set the desired width for each cell
-                    DisplayMemberBinding = new Binding("ProductName") // Adjust the binding path as needed
-                });
-            }
 
         }
     }
